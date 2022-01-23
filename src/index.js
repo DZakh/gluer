1425fdf58f54dcdf8128bc5d67f1474959565002
Dmitry Zakharov <dzakh.dev@gmail.com>
2022-01-23 15:42:13 +0300

    Add minimalistic implementation for production

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import { makeGlueTraitService } from './services/glueTraitService';
 import { makeMakeTraitService } from './services/makeTraitService';
 
 export const makeGluer = (gluerOptions) => {
+  if (process.env.NODE_ENV === 'production') {
+    return {
+      makeTrait: () => {},
+      glueTrait: () => {
+        return (impl) => {
+          return impl;
+        };
+      },
+      glueImplFactory: () => {
+        return (implFactory) => {
+          return implFactory;
+        };
+      },
+    };
+  }
+
   const makeTraitService = makeMakeTraitService();
   const glueTraitService = makeGlueTraitService({ validatePort: gluerOptions });
   const glueImplFactoryService = makeGlueImplFactoryService({ glueTraitUseCase: glueTraitService });
